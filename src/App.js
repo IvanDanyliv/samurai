b0383bd09ae75aabf680d6c40b9af4da5baabd3c
Ivan Danyliv <31760860+IvanDanyliv@users.noreply.github.com>
2022-11-23 20:16:45 +0200

    Added prors and NavLink (#2)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='app_wrapper'>
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className='app_wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app_wrapper_content'>
+          <Routes>
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Profile/MyPosts/MyPost/Post.jsx
+++ src/components/Profile/MyPosts/MyPost/Post.jsx
@@ -1,15 +1,15 @@
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div>
       <div className={s.item}>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRII7F25gIOn4UM457-mPurbQkrJSO0XhnPNQ&usqp=CAU' alt="logo" />
-        Post 1
+        {props.message}
       </div>
       <div>
-      <span>like</span>
-      </div>      
+        <span>like</span>
+      </div>
     </div>
   );
 }
