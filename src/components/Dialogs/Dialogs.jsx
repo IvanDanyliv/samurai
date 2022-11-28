61b5ca9fcaf453af79410b436d5b7debba51e41e
Ivan Danyliv <31760860+IvanDanyliv@users.noreply.github.com>
2022-11-28 17:19:47 +0200

    created layout for dialogs (#3)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,50 @@
-//import s from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
+import s from './Dialogs.module.css';
+
+const DialogItem = (props) => {
+  let path = '/dialogs/' + props.id
+
+  return (
+    <div className={s.dialog}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  )
+}
+
+const Message = (props) => {
+  return (
+    <div className={s.message}>{props.message}</div>
+  )
+}
 
 const Dialogs = (props) => {
+
+  let dialogs_data = [
+    { id: 1, name: 'Ivan' },
+    { id: 2, name: 'Taras' },
+    { id: 3, name: 'Kolia' },
+    { id: 2, name: 'Max' }
+  ]
+
+  let message_data = [
+    { id: 1, message: 'Hi' },
+    { id: 2, message: 'How' },
+    { id: 3, message: 'how how' }
+  ]
+
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogs_item}>
+        <DialogItem name={dialogs_data[0].name} id={dialogs_data[0].id} />
+        <DialogItem name='Taras' id='2' />
+        <DialogItem name='Kolia' id='3' />
+        <DialogItem name='Max' id='4' />
+      </div>
+      <div className={s.messages}>
+        <Message message={message_data[0].message} />
+        <Message message='How' />
+        <Message message='how how' />
+      </div>
     </div>
   );
 }
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,9 +1,9 @@
 import Post from './MyPost/Post';
-//import s from './MyPosts.module.css';
+import s from './MyPosts.module.css';
 
 const MyPosts = () => {
   return (
-    <div>
+    <div className={s.posts_block}>
       <Post message='Hi, how are you?' />
       <Post message='It is my first post' />
     </div>
