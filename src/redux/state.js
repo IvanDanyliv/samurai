6b5be7dd780adf5f2e2c04146d23ce00e275a931
Ivan Danyliv <danyliv04@gmail.com>
2022-12-23 20:02:28 +0200

    Upgraded state to OOP

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,46 +1,70 @@
-import { rerenderEntireTree } from "../render";
+let store = {
+  _state: {
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: 'Ivan' },
+        { id: 2, name: 'Taras' },
+        { id: 3, name: 'Kolia' },
+        { id: 4, name: 'Max' }
+      ],
 
-let state = {
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: 'Ivan' },
-      { id: 2, name: 'Taras' },
-      { id: 3, name: 'Kolia' },
-      { id: 4, name: 'Max' }
-    ],
+      messages: [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'How' },
+        { id: 3, message: 'how how' }
+      ],
+    },
 
-    messages: [
-      { id: 1, message: 'Hi' },
-      { id: 2, message: 'How' },
-      { id: 3, message: 'how how' }
-    ],
+    profilePage: {
+      posts: [
+        { id: 1, message: 'Hi, how are you?', likeCount: 10 },
+        { id: 2, message: 'It is my first post', likeCount: 7 }
+      ],
+
+      newPostText: 'samurai'
+    }
+  },
+  _callSubscriber() {
+    console.log('State changed');
   },
 
-  profilePage:{
-    posts:[
-      { id: 1, message: 'Hi, how are you?', likeCount: 10 },
-      { id: 2, message: 'It is my first post', likeCount: 7 }
-    ],
+  getState() {
+    return this._state;
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
 
-    newPostText: 'samurai'
+  // addPost() {
+  //   let newPost = {
+  //     id: 3,
+  //     message: this._state.profilePage.newPostText,
+  //     likeCount: 0
+  //   }
+  //   this._state.profilePage.posts.push(newPost);
+  //   this._state.profilePage.newPostText = '';
+  //   this._callSubscriber(this._state);
+  // },
+  // updateNewPostText(newText) {
+  //   this._state.profilePage.newPostText = newText;
+  //   this._callSubscriber(this._state);
+  // },
+  
+  dispatch(action) {
+    if (action.type === 'ADD-POST') {
+      let newPost = {
+        id: 3,
+        message: this._state.profilePage.newPostText,
+        likeCount: 0
+      }
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = '';
+      this._callSubscriber(this._state);
+    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
+    }
   }
 }
 
-export let addPost = () => {
-  let newPost = {
-    id: 3,
-    message: state.profilePage.newPostText,
-    likeCount: 0
-  }
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-}
-
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-}
-
-
-export default state;
+export default store;
