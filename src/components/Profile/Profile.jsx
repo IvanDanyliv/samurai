1e145d28758c7de4d46303058cb5af0e006b552a
Ivan Danyliv <31760860+IvanDanyliv@users.noreply.github.com>
2023-01-03 14:47:20 +0200

    Added context API (#10)

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,12 +3,12 @@ import MyPostsContainer from './MyPosts/MyPostsContainer';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 //import s from './Profile.module.css';
 
-const Profile = (props) => {
+const Profile = () => {
 
   return (
     <div>
       <ProfileInfo />
-      <MyPostsContainer store={props.store} />
+      <MyPostsContainer />
     </div>
 
   );
